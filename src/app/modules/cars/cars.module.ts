1e4741ddc30954f9da628372d93bf8d1eab0354a
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2023-12-15 22:15:55 +0200

    lesson6

--- src/app/modules/cars/cars.module.ts
+++ src/app/modules/cars/cars.module.ts
@@ -5,6 +5,13 @@ import { CarsRoutingModule } from './cars-routing.module';
 import { CarsPageComponent } from './pages/cars-page/cars-page.component';
 import { CarFormComponent } from './components/car-form/car-form.component';
 import { CarsComponent } from './components/cars/cars.component';
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
 import { CarComponent } from './components/car/car.component';
 
 
@@ -13,11 +20,18 @@ import { CarComponent } from './components/car/car.component';
     CarsPageComponent,
     CarFormComponent,
     CarsComponent,
-    CarComponent
+    CarComponent,
   ],
   imports: [
     CommonModule,
-    CarsRoutingModule
+    CarsRoutingModule,
+    MatExpansionModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ]
 })
 export class CarsModule { }
